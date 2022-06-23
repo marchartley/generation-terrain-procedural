86e339509fee714698e0dfdad70d9f25f4b9ba66
kemar74 <marc.hartley@hotmail.com>
2022-06-23 18:24:49 +0200

    Voronoi diagrams can be defined in user defined shapes

--- src/Utils/Voronoi.cpp
+++ src/Utils/Voronoi.cpp
@@ -10,7 +10,14 @@ Voronoi::Voronoi() : Voronoi(std::vector<Vector3>({}))
 
 }
 
+Voronoi::Voronoi(int numberRandomPoints, Vector3 maxBoundarie)
+    : Voronoi(numberRandomPoints, Vector3(), maxBoundarie)
+{
+
+}
+
 Voronoi::Voronoi(int numberRandomPoints, Vector3 minBoundarie, Vector3 maxBoundarie)
+    : minBoundarie(minBoundarie), maxBoundarie(maxBoundarie)
 {
     for (int i = 0; i < numberRandomPoints; i++) {
         pointset.push_back(Vector3(random_gen::generate(minBoundarie.x, maxBoundarie.x),
@@ -18,15 +25,42 @@ Voronoi::Voronoi(int numberRandomPoints, Vector3 minBoundarie, Vector3 maxBounda
     }
 }
 
+Voronoi::Voronoi(int numberRandomPoints, ShapeCurve boundingShape)
+{
+    std::tie(this->minBoundarie, this->maxBoundarie) = boundingShape.AABBox();
+    this->pointset = boundingShape.randomPointsInside(numberRandomPoints);
+}
+
 Voronoi::Voronoi(std::vector<Vector3> pointset)
     : pointset(pointset)
 {
+    minBoundarie = Vector3::min();
+    maxBoundarie = Vector3::max();
+    for (auto& point : pointset) {
+        minBoundarie.x = std::min(point.x, minBoundarie.x);
+        minBoundarie.y = std::min(point.y, minBoundarie.y);
+        minBoundarie.z = std::min(point.z, minBoundarie.z);
+        maxBoundarie.x = std::max(point.x, maxBoundarie.x);
+        maxBoundarie.y = std::max(point.y, maxBoundarie.y);
+        maxBoundarie.z = std::max(point.z, maxBoundarie.z);
+    }
+}
+
+Voronoi::Voronoi(std::vector<Vector3> pointset, Vector3 maxBoundarie)
+    : Voronoi(pointset)
+{
+    this->maxBoundarie = maxBoundarie;
+}
+
+Voronoi::Voronoi(std::vector<Vector3> pointset, Vector3 minBoundarie, Vector3 maxBoundarie)
+    : Voronoi(pointset)
+{
+    this->minBoundarie = minBoundarie;
+    this->maxBoundarie = maxBoundarie;
 }
 
 std::vector<BSpline> Voronoi::solve()
 {
-    std::vector<BSpline> areas;
-
     jcv_point* points = (jcv_point*)malloc( sizeof(jcv_point) * pointset.size());
     jcv_point* boundingPoints = (jcv_point*)malloc( sizeof(jcv_point) * boundingShape.points.size());
     for (size_t i = 0; i < pointset.size(); i++) {
@@ -37,7 +71,6 @@ std::vector<BSpline> Voronoi::solve()
         boundingPoints[i].x = boundingShape.points[i].x;
         boundingPoints[i].y = boundingShape.points[i].y;
     }
-
 
     jcv_clipper polygonclipper;
     jcv_clipper* clipper = 0;
@@ -56,39 +89,36 @@ std::vector<BSpline> Voronoi::solve()
         // nada
     }
 
+    jcv_rect* rect = new jcv_rect;
+    rect->min = {minBoundarie.x, minBoundarie.y};
+    rect->max = {maxBoundarie.x, maxBoundarie.y};
+
     jcv_diagram diagram;
     memset(&diagram, 0, sizeof(jcv_diagram));
-    jcv_diagram_generate(pointset.size(), (const jcv_point*)points, NULL, clipper, &diagram);
+    jcv_diagram_generate(pointset.size(), (const jcv_point*)points, rect, clipper, &diagram);
 
     const jcv_site* sites = jcv_diagram_get_sites( &diagram );
+    std::vector<BSpline> areas(pointset.size());
     for( int i = 0; i < diagram.numsites; ++i )
     {
         const jcv_site* site = &sites[i];
         BSpline areaShape;
 
-//        jcv_point s = site->p; // remap(&site->p, &diagram.min, &diagram.max, &dimensions );
-
         const jcv_graphedge* e = site->edges;
-//        std::cout << "Area #" << (i + 1) << "\n";
         while( e )
         {
-            jcv_point p0 = e->pos[0]; // remap(&e->pos[0], &diagram.min, &diagram.max, &dimensions );
-//            jcv_point p1 = e->pos[1]; // remap(&e->pos[1], &diagram.min, &diagram.max, &dimensions );
-
+            jcv_point p0 = e->pos[0];
             Vector3 v0 = Vector3(p0.x, p0.y);
-//            Vector3 v1 = Vector3(p1.x, p1.y);
-
-//            std::cout << v0 << " - " << v1 << "\n";
             areaShape.points.push_back(v0);
             e = e->next;
         }
-//        std::cout << std::endl;
-        areas.push_back(areaShape);
+        areas[site->index] = areaShape;
     }
 
     jcv_diagram_free(&diagram);
     free(points);
     free(boundingPoints);
+    delete rect;
 
     return areas;
 }
--- src/Utils/Voronoi.h
+++ src/Utils/Voronoi.h
@@ -4,14 +4,19 @@
 
 #include "DataStructure/Vector3.h"
 #include "Utils/BSpline.h"
+#include "Utils/ShapeCurve.h"
 #include <vector>
 
 class Voronoi
 {
 public:
     Voronoi();
+    Voronoi(int numberRandomPoints, Vector3 maxBoundarie);
     Voronoi(int numberRandomPoints, Vector3 minBoundarie, Vector3 maxBoundarie);
+    Voronoi(int numberRandomPoints, ShapeCurve boundingShape);
     Voronoi(std::vector<Vector3> pointset);
+    Voronoi(std::vector<Vector3> pointset, Vector3 maxBoundarie);
+    Voronoi(std::vector<Vector3> pointset, Vector3 minBoundarie, Vector3 maxBoundarie);
     std::vector<BSpline> solve();
 
 
@@ -19,6 +24,7 @@ public:
     std::vector<Vector3> pointset;
     std::vector<std::vector<Vector3>> intersectionPoints;
     BSpline boundingShape;
+    Vector3 minBoundarie, maxBoundarie;
 };
 
 #endif // VORONOI_H
